5abe6dead57ee9d693d35ee7a1f7e6eff7ffc4ea
Shiva Rama Krishna-shetty <150837518+shetty1845@users.noreply.github.com>
2026-04-27 11:58:17 +0530

    Update sns_service to use IAM role and add error handling
    Refactor SNS client initialization to use IAM role instead of access keys and add error handling for publish method.

--- warehouse-alert-system/services/sns_service.py
+++ warehouse-alert-system/services/sns_service.py
@@ -1,16 +1,18 @@
 import boto3
 from config import Config
 
+# ✅ NO KEYS (IAM ROLE USED)
 sns = boto3.client(
     'sns',
-    aws_access_key_id=Config.AWS_ACCESS_KEY,
-    aws_secret_access_key=Config.AWS_SECRET_KEY,
     region_name=Config.REGION
 )
 
 def send_alert(message):
-    sns.publish(
-        TopicArn=Config.SNS_TOPIC_ARN,
-        Message=message,
-        Subject="Low Stock Alert"
-    )
+    try:
+        sns.publish(
+            TopicArn=Config.SNS_TOPIC_ARN,
+            Message=message,
+            Subject="Low Stock Alert"
+        )
+    except Exception as e:
+        print("SNS Error:", e)
